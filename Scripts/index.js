e98a3de04d6570d625b6249f340d79c53e6ce4ea
nshupinski <55292866+nshupinski@users.noreply.github.com>
2021-11-03 15:57:46 -0400

    Added Project modal functions and background darken and blur when clicked.

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,3 +3,48 @@ function btnAccord_Clicked() {
     var btn = document.getElementById("btnAccord");
     btn.setAttribute("style", "background-color: #a6b6b4");
 }
+
+
+/* Show Project Modals */
+function showProj1Modal() {
+    document.getElementById('proj1Modal').style.display = "block";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center is-blurred";
+    document.getElementById('page-mask').style.zIndex = "10";
+}
+function showProj2Modal() {
+    document.getElementById('proj2Modal').style.display = "block";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center is-blurred";
+    document.getElementById('page-mask').style.zIndex = "10";
+}
+function showProj3Modal() {
+    document.getElementById('proj3Modal').style.display = "block";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center is-blurred";
+    document.getElementById('page-mask').style.zIndex = "10";
+}
+function showProj4Modal() {
+    document.getElementById('proj4Modal').style.display = "block";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center is-blurred";
+    document.getElementById('page-mask').style.zIndex = "10";
+}
+
+/* Hide Project Modals */
+function hideProj1Modal() {
+    document.getElementById('proj1Modal').style.display = "none";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center";
+    document.getElementById('page-mask').style.zIndex = "-10";
+}
+function hideProj2Modal() {
+    document.getElementById('proj2Modal').style.display = "none";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center";
+    document.getElementById('page-mask').style.zIndex = "-10";
+}
+function hideProj3Modal() {
+    document.getElementById('proj3Modal').style.display = "none";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center";
+    document.getElementById('page-mask').style.zIndex = "-10";
+}
+function hideProj4Modal() {
+    document.getElementById('proj4Modal').style.display = "none";
+    document.getElementById('headerBackground').className = "bg-img justify-content-center";
+    document.getElementById('page-mask').style.zIndex = "-10";
+}
