009c1ea1df55a4227d031fc63824bc0e85914f94
nshupinski <55292866+nshupinski@users.noreply.github.com>
2023-11-26 22:45:22 -0500

    Updated projects and headshot

---
+++ Game/scripts/game_script.js
@@ -0,0 +1,39 @@
+window.addEventListener("keydown", function(e) {
+    if(["ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+
+
+
+var character = document.getElementById("character");
+var interval;  
+var both = 0;
+
+function moveLeft() {
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left - 2 + "px";
+}
+
+function moveRight() {
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left + 2 + "px";
+}
+
+document.addEventListener("keydown", event => {
+    if(both == 0) {
+        both++;
+        if(event.key==="ArrowLeft") {
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key==="ArrowRight") {
+            interval = setInterval(moveRight, 1);
+        }
+    }
+})
+
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
+    both = 0;
+})
